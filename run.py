57e1ee5001ab46e33c48c7ccab1d5b729fc5fbee
Ko Phyo <43138249+K0PHY03@users.noreply.github.com>
2021-08-16 13:51:49 +0630

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+cline=raw_input('\033[1;33;40mEnter Cline ( Start with C: ) :\033[1;36;40m ');
+
+if __name__ == "__main__":
+    import CCcamTester
+    CCcamTester.TestCline(""+cline)
